3c6ac91324fef2c9849cd01a76bb03ded21dc3df
Alex Sorafumo <alex@yuion.net>
2021-07-15 10:38:04 +1000

    fix: tweak to previous commit

--- src/utilities/abort-controller.ts
+++ src/utilities/abort-controller.ts
@@ -3,8 +3,6 @@ export class AbortControllerStub {
     public abort() {}
 }
 
-const global_space = window || global;
-
-if (!global_space.AbortController) {
-    (global_space as any).AbortController = AbortControllerStub;
+if (!globalThis.AbortController) {
+    (globalThis as any).AbortController = AbortControllerStub;
 }
