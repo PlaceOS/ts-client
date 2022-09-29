0968fa03b081684e343eb44f6cb993868d677493
Alex Sorafumo <alex@yuion.net>
2022-09-29 22:54:46 +1000

    fix(edge): add field for x api key

--- src/edge/edge.ts
+++ src/edge/edge.ts
@@ -3,12 +3,13 @@ import { PlaceResource } from '../resources/resource';
 export class PlaceEdge extends PlaceResource {
 
     public readonly description: string;
-
     public readonly secret: string;
+    public readonly x_api_key: string;
 
     constructor(raw_data: Partial<PlaceEdge> = {}) {
         super(raw_data);
         this.description = raw_data.description || '';
         this.secret = raw_data.secret || '';
+        this.x_api_key = raw_data.x_api_key || '';
     }
 }
