2797c0264335475bc457526c992f65cd61261383
Marvin Yoong <marvin@place.technology>
2023-01-16 13:01:54 +1100

    feat: question and survey modal updates

--- src/staff/questions/model.ts
+++ src/staff/questions/model.ts
@@ -3,20 +3,20 @@ export class SurveyQuestion {
     title: string;
     description?: string;
     type: string;
-    question_json: Record<string, any>;
-    required?: boolean;
-    max_rating?: number;
-    choices?: Array<any>;
-    tags?: Array<string>;
+    options: any;
+    required: boolean;
+    max_rating: number;
+    choices: Array<any>;
+    tags: Array<string>;
 
     constructor(_data: Partial<SurveyQuestion>) {
         this.id = _data.id || 0;
         this.title = _data.title || '';
         this.description = _data.description || '';
         this.type = _data.type || '';
-        this.question_json = _data.question_json || {};
+        this.options = _data.options || {};
         this.required = _data.required || false;
-        this.max_rating = _data.max_rating;
+        this.max_rating = _data.max_rating || 0;
         this.choices = _data.choices || [];
         this.tags = _data.tags || [];
     }
