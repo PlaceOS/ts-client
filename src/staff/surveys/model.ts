e931b9282674b69e58cf157aef659e59652b51f3
Marvin Yoong <marvin@place.technology>
2023-01-17 14:45:29 +1100

    feat: update surveys modal for multi page support

--- src/staff/surveys/model.ts
+++ src/staff/surveys/model.ts
@@ -2,14 +2,22 @@ export class Survey {
     public readonly id: number;
     public readonly title: string;
     public readonly description?: string;
-    public readonly question_order: Array<number>;
     public readonly trigger: string;
+    public readonly zone_id: string;
+    public readonly pages: SurveyPage[];
 
     constructor(_data: Partial<Survey>) {
         this.id = _data.id || 0;
         this.title = _data.title || '';
         this.description = _data.description || '';
-        this.question_order = _data.question_order || [];
+        this.zone_id = _data.zone_id || '';
+        this.pages = _data.pages || [];
         this.trigger = _data.trigger || 'NONE';
     }
+}
+
+export interface SurveyPage {
+    title: string;
+    description?: string;
+    question_order: number[]; 
 }
