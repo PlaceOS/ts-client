7bce430c08f8c4f2062ddaa4339ec7554b3e8851
Marvin Yoong <marvin@place.technology>
2023-01-18 15:46:41 +1100

    feat(staff-api): Add new building_id field

--- src/staff/surveys/model.ts
+++ src/staff/surveys/model.ts
@@ -3,6 +3,7 @@ export class Survey {
     public readonly title: string;
     public readonly description?: string;
     public readonly trigger: string;
+    public readonly building_id: string;
     public readonly zone_id: string;
     public readonly pages: SurveyPage[];
 
@@ -10,6 +11,7 @@ export class Survey {
         this.id = _data.id || 0;
         this.title = _data.title || '';
         this.description = _data.description || '';
+        this.building_id = _data.building_id || '';
         this.zone_id = _data.zone_id || '';
         this.pages = _data.pages || [];
         this.trigger = _data.trigger || 'NONE';
