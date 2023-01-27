5308e677faf5510de7d6e60afe9f6496edea9d5b
Marvin Yoong <marvin@place.technology>
2023-01-27 15:17:05 +1100

    fix(survey): fix incorrect translation

--- src/staff/answers/model.ts
+++ src/staff/answers/model.ts
@@ -7,7 +7,7 @@ export class SurveyAnswer {
 
     constructor(_data: Partial<SurveyAnswer>) {
         this.id = _data.id || 0;
-        this.question_id = _data.id || 0;
+        this.question_id = _data.question_id || 0;
         this.survey_id = _data.survey_id || 0;
         this.type = _data.type || '';
         this.answer_json = _data.answer_json || {};
