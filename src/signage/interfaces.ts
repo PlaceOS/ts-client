4b201f1a65b247d2d4fda9bb0a7b2efa648e7506
Alex Sorafumo <alex@yuion.net>
2026-04-12 15:22:40 +1000

    fix(signage): add preview and item_id query params to show media method

--- src/signage/interfaces.ts
+++ src/signage/interfaces.ts
@@ -10,10 +10,15 @@ export interface SignageMetrics {
 export interface SignageMediaQueryOptions extends PlaceResourceQueryOptions {
     /** ID of the authority to filter the returned values on */
     authority_id?: string;
+    /** Ignore state changes to the display media is requested for */
+    preview?: boolean;
+    /** ID of the currently playing item */
+    item_id?: string;
 }
 
 /** Allowable query parameters for signage playlists index endpoint */
-export interface SignagePlaylistQueryOptions extends PlaceResourceQueryOptions {}
+export interface SignagePlaylistQueryOptions
+    extends PlaceResourceQueryOptions {}
 
 /** Allowable query parameters for signage playlist media revisions endpoint */
 export interface SignagePlaylistRevisionsOptions {
